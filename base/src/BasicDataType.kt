83b18a52019351ea8e2c3dff0d3bb63e961abac8
cjx <1067446576@qq.com>
2020-03-29 00:44:50 +0800

    ignore out

---
+++ base/src/BasicDataType.kt
@@ -0,0 +1,7 @@
+package com.cjx
+
+/**
+ * @Auther: cjx
+ * @Date: 2020/3/28 21:11
+ * @Description:
+ */
--- base/src/app.kt
+++
@@ -1,7 +0,0 @@
-/**
- * Created by chenjinxin on 2020-03-25 14:58
- */
-
-fun main(args: Array<String>) {
-    println("Hello World");
-}
